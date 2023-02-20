16bbe97baba0a0ee448382c86544cb1054e88e15
pablovisual <pabloarmandosandoval@gmail.com>
2023-02-20 22:10:35 +0000

    lab complete

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,11 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+        for (int index = 0; index < arr.length; ++index) {
+            if (arr[index] == target) 
+                return true;
+        }
+
         return false;
     }
 }
